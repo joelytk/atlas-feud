329eda6dabf31f06e88e4e0e4922c0d196dd8c2c
Joel Yap <joelytk@gmail.com>
2024-08-18 21:51:16 +0800

    Moved sound logic to custom hook

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,48 +3,61 @@ import { useEffect, useState } from 'react';
 import AnswerType from '../types/Answer';
 
 import AnswerGrid from './AnswerGrid';
-import Audio from './Audio';
 import Question from './Question';
 import ScoreCounter from './ScoreCounter';
 
+import useAudio from '../hooks/useAudio';
+
 const Board = () => {
-  const [score, setScore] = useState<number>(0);
-  const [question, setQuestion] = useState<string>('');
-  const [answers, setAnswers] = useState([]);
-  const [completed, setCompleted] = useState<number[]>([]);
-  const [sound, setSound] = useState<string>('');
+	const [score, setScore] = useState<number>(0);
+	const [question, setQuestion] = useState<string>('');
+	const [answers, setAnswers] = useState([]);
+	const [completed, setCompleted] = useState<number[]>([]);
+	const { audioRef, playSound } = useAudio();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch('/surveys.json');
-        const data = await res.json();
-        const randomIndex = Math.floor(Math.random() * data.season23?.length);
-        const survey = data.season23[randomIndex];
-        setQuestion(survey.question);
-        setAnswers(survey.answers);
-      } catch (error) {
-        console.error({ error });
-      }
-    };
+	useEffect(() => {
+		playSound('intro');
+	}, []);
 
-    fetchData();
-  }, []);
+	useEffect(() => {
+		if (completed.length > 0 && completed.length === answers.length) {
+			playSound('round-win');
+		}
+	}, [completed]);
 
-  const addScore = (answer: AnswerType) => {
-    setScore(score + answer.score);
-    setCompleted([...completed, answer.id]);
-    setSound('correct');
-  };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await fetch('/surveys.json');
+				const data = await res.json();
+				const randomIndex = Math.floor(Math.random() * data.season23?.length);
+				const survey = data.season23[randomIndex];
+				setQuestion(survey.question);
+				setAnswers(survey.answers);
+			} catch (error) {
+				console.error({ error });
+			}
+		};
 
-  return (
-    <div className='container'>
-      <Question question={question} />
-      <ScoreCounter score={score} />
-      <AnswerGrid answers={answers} completed={completed} addScore={addScore} />
-      <Audio sound={sound} setSound={setSound} />
-    </div>
-  );
+		fetchData();
+	}, []);
+
+	const addScore = (answer: AnswerType) => {
+		setScore(score + answer.score);
+		setCompleted([...completed, answer.id]);
+		playSound('correct');
+	};
+
+	return (
+		<div className='container'>
+			<Question question={question} />
+			<ScoreCounter score={score} />
+			<AnswerGrid answers={answers} completed={completed} addScore={addScore} />
+			<audio ref={audioRef}>
+				Your browser does not support the audio element.
+			</audio>
+		</div>
+	);
 };
 
 export default Board;
---
+++ src/hooks/useAudio.ts
@@ -0,0 +1,47 @@
+import { useRef } from 'react';
+
+import Buzzer from '../assets/sounds/buzzer.mp3';
+import Correct from '../assets/sounds/correct.mp3';
+import Intro from '../assets/sounds/intro.mp3';
+import Review from '../assets/sounds/review.mp3';
+import RoundWin from '../assets/sounds/round-win.mp3';
+import Wrong from '../assets/sounds/wrong.mp3';
+
+const useAudio = () => {
+	const audioRef = useRef();
+
+	const playSound = (sound: string) => {
+		const audioElement = audioRef.current;
+		let soundFile = null;
+
+		switch (sound) {
+			case 'buzzer':
+				soundFile = Buzzer;
+				break;
+			case 'correct':
+				soundFile = Correct;
+				break;
+			case 'intro':
+				soundFile = Intro;
+				break;
+			case 'review':
+				soundFile = Review;
+				break;
+			case 'round-win':
+				soundFile = RoundWin;
+				break;
+			case 'wrong':
+				soundFile = Wrong;
+				break;
+			default:
+				break;
+		}
+
+		audioElement.src = soundFile;
+		audioElement.play();
+	};
+
+	return { audioRef, playSound };
+};
+
+export default useAudio;
