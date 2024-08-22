d5fdf348258c65c6627b6816adbf6550bc30adcb
Joel Yap <joelytk@gmail.com>
2024-08-22 10:04:19 +0800

    Added base

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+	plugins: [react()],
+	server: {
+		port: 3000,
+		open: true
+	},
+	base: 'https://joelytk.github.io/atlas-feud/'
 });
